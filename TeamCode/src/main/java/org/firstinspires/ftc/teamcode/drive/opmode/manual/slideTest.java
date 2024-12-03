b76ea41ed891c29fc3fb9795dabf48807e3322a1
Rymantheman <Rymantheman@yahoo.com>
2024-12-03 18:13:01 -0500

    Added Test Manuals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/manual/slideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/manual/slideTest.java
@@ -4,16 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="ManualSlopMode", group="practice")
-public class manualTest extends OpMode{
-
-    // Create motor & servo variables
-    DcMotor leftFront, rightFront, leftBack, rightBack; // Drive Motors
+@TeleOp(name = "SlideControl")
+public class slideTest extends OpMode {
     DcMotor leftSlide, rightSlide, leftSlidePivot, rightSlidePivot; // Slide Motors
-
-    Servo leftClaw, rightClaw; // Claw Servos
 
     double leftSlideStartPos;
     double rightSlideStartPos;
@@ -22,20 +16,6 @@ public class manualTest extends OpMode{
     double rightSlidePivStartPos;
 
     public void init() {
-        // Drive Motors
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        // leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        rightFront = hardwareMap.dcMotor.get("rightFront");
-        // rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        // leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        rightBack = hardwareMap.dcMotor.get("rightBack");
-        // rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // Slide Motors
         leftSlide = hardwareMap.dcMotor.get("leftSlide");
         leftSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
@@ -48,57 +28,22 @@ public class manualTest extends OpMode{
         rightSlidePivot = hardwareMap.dcMotor.get("rightSlidePivot");
         rightSlidePivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Claw Servos
-        leftClaw = hardwareMap.servo.get("leftClaw");
-        rightClaw = hardwareMap.servo.get("rightClaw");
-
-        // Set the slide motors to brake when no power is applied
         leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftSlidePivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightSlidePivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // TODO: Reverse Necessary Motors
-        leftSlidePivot.setDirection(DcMotorSimple.Direction.REVERSE); // Reverse left pivot motor
+        leftSlidePivot.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // Starting positions of slide and pivot motors
         leftSlideStartPos = leftSlide.getCurrentPosition();
         rightSlideStartPos = rightSlide.getCurrentPosition();
 
         leftSlidePivStartPos = leftSlidePivot.getCurrentPosition();
         rightSlidePivStartPos = rightSlidePivot.getCurrentPosition();
+
     }
-
     public void loop() {
-
-        // TODO: Program Gamepad 1
-        // Controls the robot's movement
-
-        double x = gamepad1.left_stick_x;
-        double y = -gamepad1.left_stick_y;
-        double rotInput = gamepad1.left_trigger - gamepad1.right_trigger;
-
-        leftFront.setPower((y + x - rotInput));
-        leftBack.setPower((-y + x - rotInput));
-        rightFront.setPower((y - x + rotInput));
-        rightBack.setPower((y + x + rotInput));
-
-        // TODO: Program Gamepad 2
-        // Controls the ViperSlides
-
-        // TODO: Program Claw
-
-
-        double vert = gamepad2.left_stick_y; // Vertical height
-
-        if (leftSlide.getCurrentPosition() > (leftSlideStartPos + 2000) || rightSlide.getCurrentPosition() < (rightSlideStartPos - 2000)) {
-            leftSlide.setPower(-vert);
-            rightSlide.setPower(vert);
-        }
-        else {
-            leftSlide.setPower(-vert);
-            rightSlide.setPower(vert);
-        }
-
         telemetry.addData("Left Slide Position", leftSlide.getCurrentPosition());
         telemetry.addData("Right Slide Position", rightSlide.getCurrentPosition());
 
@@ -109,11 +54,11 @@ public class manualTest extends OpMode{
 
         if (leftSlidePivot.getCurrentPosition() > (leftSlidePivStartPos + 1000) || rightSlidePivot.getCurrentPosition() <= (rightSlidePivStartPos - 2000)) {
             leftSlidePivot.setPower(pivot);
-            rightSlidePivot.setPower(-pivot);
+            rightSlidePivot.setPower(-pivot*.5);
         }
         else {
             leftSlidePivot.setPower(pivot);
-            rightSlidePivot.setPower(-pivot);
+            rightSlidePivot.setPower(-pivot *.5);
         }
     }
 }
